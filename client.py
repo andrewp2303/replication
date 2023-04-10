de612774e9044a05b0e317de808ac8008c01ae83
Adarsh Hiremath <ahiremath35@gmail.com>
2023-04-09 20:08:36 -0400

    Update client.py
    Now it's working for fault tolerance

--- client.py
+++ client.py
@@ -4,48 +4,28 @@ import select
 from termcolor import colored
 import threading
 
-def conn_threads(conn):
-
-    # Main loop for clients to receive and send messages to the server.
-    while True:   
-        # List of input streams.
-        sockets_list = [sys.stdin, conn]
-
-        # Initialize read sockets to process inputs from the server.
-        read_sockets, _, _ = select.select(
-            sockets_list, [], [])
-
-        for socks in read_sockets:
-
-            # Display messages received from the server.
-            if socks == conn:
-                msg = socks.recv(4096)
-                print(msg.decode('UTF-8'))
-
-            # Send requests to the server from the client.
-            else:
-                msg = sys.stdin.readline().strip()
-                conn.send(msg.encode('UTF-8'))
-                data = conn.recv(4096)
-                print(str(data.decode('UTF-8')))
+def check_alive_conns(conn_list):
+    alive_conns = []
+    for elt in conn_list: 
+        try:
+            elt.send(b"")
+            alive_conns.append(elt)
+        except:
+            continue
+    return alive_conns
 
 def Main():
-    servers = [None, None, None]
+    conn1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    conn1.connect(("localhost", 5050))
 
-    for i in range(3):
-        # Create a TCP socket connection.
-        servers[i] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        servers[i].setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
-        # Connect to the servers at the appropriate IP address. 
-        if (i == 0): 
-            servers[i].connect(("localhost", 5050))
+    conn2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    conn2.connect(("localhost", 5051))
 
-        elif (i == 1): 
-            servers[i].connect(("localhost", 5051))
-
-        elif (i == 2):
-            servers[i].connect(("localhost", 5052))
+    conn3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn3.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    conn3.connect(("localhost", 5052))
 
     # Welcome message.
     msg = "\nWelcome to the chat application! Begin by logging in or creating an account. Below, you will find a list of supported commands :\n"
@@ -58,19 +38,30 @@ def Main():
     msg += "\nUsage help (this page).   h\n"
     msg = colored(msg, 'yellow')
     print(msg)
- 
     
-    listener1 = threading.Thread(target=conn_threads, args=(servers[0],))
-    listener2 = threading.Thread(target=conn_threads, args=(servers[1],))
-    listener3 = threading.Thread(target=conn_threads, args=(servers[2],))
+    # Main loop for clients to receive and send messages to the server.
+    while True:   
+        alive_conns = check_alive_conns([conn1, conn2, conn3])
+        curr_conn = alive_conns[0]
 
-    listener1.start()
-    listener2.start()
-    listener3.start()
+        # List of input streams.
+        sockets_list = [sys.stdin, curr_conn]
 
-    listener1.join()
-    listener2.join()
-    listener3.join()
+        # Initialize read sockets to process inputs from the server.
+        read_sockets, _, _ = select.select(sockets_list, [], [])
+
+        for socks in read_sockets:
+            # Display messages received from the server.
+            if socks == curr_conn:
+                msg = socks.recv(4096)
+                print(msg.decode('UTF-8'))
+
+            # Send requests to the server from the client.
+            else:
+                msg = sys.stdin.readline().strip()
+                curr_conn.send(msg.encode('UTF-8'))
+                data = curr_conn.recv(4096)
+                print(str(data.decode('UTF-8')))
 
 if __name__ == '__main__':
     Main()
